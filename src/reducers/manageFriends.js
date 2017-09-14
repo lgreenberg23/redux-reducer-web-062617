84aac28ae9f3fe9e463569d7071fc77f9cb3ffb4
github username <github email address>
2017-09-14 09:27:45 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,38 @@
 export function manageFriends(state, action){
+	switch (action.type){
+		case 'ADD_FRIEND':
+			return {friends: state.friends.concat(action.friend)}
+			break;
+		case 'REMOVE_FRIEND':
+
+
+
+
+			//return {state.friends.slice(0, state.length-1)}
+			return {friends: state.friends.filter(obj => obj.id !== action.id)}
+			break;
+		default:
+			return state
+	}
+
 }
+
+
+// let dog = {id: 1, name: 'scooby', color: 'brown', age: 4};
+ 
+// let olderDog = {...dog, age: dog.age + 1}
+
+// function reducer(state, action){      
+//   switch (action.type) {
+//     case 'INCREASE_COUNT':
+//       return {count: state.count + 1}
+//     case 'DECREASE_COUNT'
+//       return {count: state.count - 1}
+//     default:
+//       return state;
+//   }
+// }
+
+// Lets write a function that will help us manage our gift recipient list. 
+// We should be able to add a person we need to buy a present for and remove 
+// people we no longer like (or who give us socks every year!).
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,30 @@
 export function managePresents(state, action){
+		switch (action.type){
+		case 'INCREASE':
+			return {numberOfPresents: state.numberOfPresents + 1}
+		default:
+			return state
+	}
 }
+
+
+
+
+// let dog = {id: 1, name: 'scooby', color: 'brown', age: 4};
+ 
+// let olderDog = {...dog, age: dog.age + 1}
+
+// function reducer(state, action){      
+//   switch (action.type) {
+//     case 'INCREASE_COUNT':
+//       return {count: state.count + 1}
+//     case 'DECREASE_COUNT'
+//       return {count: state.count - 1}
+//     default:
+//       return state;
+//   }
+// }
+
+// Lets write a function that will help us manage our gift recipient list. 
+// We should be able to add a person we need to buy a present for and remove 
+// people we no longer like (or who give us socks every year!).
